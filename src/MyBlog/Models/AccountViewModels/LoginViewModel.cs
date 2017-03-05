e080e15fc30b20ed9e3fea80463b784c330594b3
胡玮文 <huww98@163.com>
2017-03-05 14:00:00 +0800

    调整界面，简化补全脚本，升级程序包

--- src/MyBlog/Models/AccountViewModels/LoginViewModel.cs
+++ src/MyBlog/Models/AccountViewModels/LoginViewModel.cs
@@ -9,11 +9,12 @@ namespace MyBlog.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        [Display(Name ="E-mail/用户名")]
+        [Display(Name = "E-mail/用户名")]
         public string EmailOrUserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
         [Display(Name = "记住我?")]
